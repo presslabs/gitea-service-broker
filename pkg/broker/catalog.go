a6ac62a5eed5c9919bd07d43b01a7e9aa4c1db97
Calin Don <calin.don@gmail.com>
2019-03-12 22:11:50 +0200

    Generate json schema for provision/binding

--- pkg/broker/catalog.go
+++ pkg/broker/catalog.go
@@ -2,8 +2,10 @@ package broker
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
+	"github.com/alecthomas/jsonschema"
 	"github.com/pivotal-cf/brokerapi"
 
 	"github.com/presslabs/gitea-service-broker/pkg/cmd/options"
@@ -11,18 +13,43 @@ import (
 
 var boolTrue = true
 
-var defaultPlan = brokerapi.ServicePlan{
-	ID:          options.DefaultPlanID,
-	Name:        options.DefaultPlanName,
-	Description: "Creates a private repository for an organization",
-	Free:        &boolTrue,
-	Metadata: &brokerapi.ServicePlanMetadata{
-		Bullets:     []string{},
-		DisplayName: "Default",
-	},
+var reflector = jsonschema.Reflector{
+	ExpandedStruct: true,
 }
 
-var planList = []brokerapi.ServicePlan{defaultPlan}
+var repoJSONSchemaBytes, _ = json.Marshal(reflector.Reflect(&ProvisionParameters{}))
+var deployKeyJSONSchemaBytes, _ = json.Marshal(reflector.Reflect(&BindingParameters{}))
+
+func (giteaServiceBroker *GiteaServiceBroker) repoPlans() []brokerapi.ServicePlan {
+	plans := make([]brokerapi.ServicePlan, 1)
+
+	repoSchema := brokerapi.Schema{}
+	json.Unmarshal(repoJSONSchemaBytes, &repoSchema.Parameters)
+
+	deployKeySchema := brokerapi.Schema{}
+	json.Unmarshal(deployKeyJSONSchemaBytes, &deployKeySchema.Parameters)
+
+	plans[0] = brokerapi.ServicePlan{
+		ID:          options.DefaultPlanID,
+		Name:        options.DefaultPlanName,
+		Description: "Creates a private repository for an organization",
+		Free:        &boolTrue,
+		Metadata: &brokerapi.ServicePlanMetadata{
+			Bullets:     []string{},
+			DisplayName: "Default",
+		},
+		Schemas: &brokerapi.ServiceSchemas{
+			Instance: brokerapi.ServiceInstanceSchema{
+				Create: repoSchema,
+			},
+			Binding: brokerapi.ServiceBindingSchema{
+				Create: deployKeySchema,
+			},
+		},
+	}
+
+	return plans
+}
 
 // Services returns a list of brokered services
 func (giteaServiceBroker *GiteaServiceBroker) Services(ctx context.Context) ([]brokerapi.Service, error) {
@@ -32,7 +59,7 @@ func (giteaServiceBroker *GiteaServiceBroker) Services(ctx context.Context) ([]b
 			Name:        options.ServiceName,
 			Description: fmt.Sprintf("Creates git repositories on Gitea at %s", options.GiteaURL),
 			Bindable:    true,
-			Plans:       planList,
+			Plans:       giteaServiceBroker.repoPlans(),
 			Metadata: &brokerapi.ServiceMetadata{
 				DisplayName: "Git Repository",
 			},
